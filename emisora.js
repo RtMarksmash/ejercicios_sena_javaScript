6ebc57a1de29563b91c88fd531ce38d27dd4321d
mario mora <jorgemariomoramolina@gmail.com>
2025-08-10 07:34:49 -0500

    continuando con el programa de la emisora sena

--- emisora.js
+++ emisora.js
@@ -7,8 +7,20 @@ let RadioEscuchas = [];
 
 function insertarPersonas() {
 
-    let input = document.getElementById("inputVectores");
-    const integer = parseInt(input.value)
+    let inputNombre = document.getElementById("nombre");
+    const nombre = inputNombre.value
+    let inputCedula = document.getElementById("cedula");
+    const cedula = parseInt(inputCedula.value)
+    let inputFecha = document.getElementById("fecha");
+    const fecha = inputFecha.value
+    let inputEmail = document.getElementById("email");
+    const email = inputEmail.value
+    let inputResidencia = document.getElementById("inputVectores");
+    const residencia = inputResidencia.value
+    let inputOrigen = document.getElementById("inputVectores");
+    const origen = inputOrigen.value
+
+
 
 
     const persona = {

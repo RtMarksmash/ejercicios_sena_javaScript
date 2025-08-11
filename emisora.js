4b12837268a66ac7b48c40351b33215cdec22943
jorge mario mora <jorgemariomoramolina@gmail.com>
2025-08-11 06:12:48 -0500

    terminando otra vez el ultimo ejercisio

--- emisora.js
+++ emisora.js
@@ -7,6 +7,8 @@ let RadioEscuchas = [];
 
 function insertarPersonas() {
 
+    const id = Math.floor(Math.random() * 10000) + 1;
+
     let inputNombre = document.getElementById("nombre");
     const nombre = inputNombre.value
     let inputCedula = document.getElementById("cedula");
@@ -15,24 +17,36 @@ function insertarPersonas() {
     const fecha = inputFecha.value
     let inputEmail = document.getElementById("email");
     const email = inputEmail.value
-    let inputResidencia = document.getElementById("inputVectores");
+    let inputResidencia = document.getElementById("residencia");
     const residencia = inputResidencia.value
-    let inputOrigen = document.getElementById("inputVectores");
+    let inputOrigen = document.getElementById("origen");
     const origen = inputOrigen.value
-
-
+    let inputArtista = document.getElementById("artista");
+    const artista = inputArtista.value
+    let inputFavorito1 = document.getElementById("favorito1");
+    const favorito1 = inputFavorito1.value
+    let inputFavorito2 = document.getElementById("favorito2");
+    const favorito2 = inputFavorito2.value
+    let inputFavorito3 = document.getElementById("favorito3");
+    const favorito3 = inputFavorito3.value
 
 
     const persona = {
-        id: 1,
-        nombre: "jorge",
-        fechaDeNacimiento: "22-10-2009",
-        email: "jdfakhf@adhfasdhf",
-        ciudadDeResidencia: "cucuta",
-        ciudadDeOrigen: "cucuta",
+        id: id,
+        nombre: nombre,
+        cedula: cedula,
+        fechaDeNacimiento: fecha,
+        email: email,
+        ciudadDeResidencia: residencia,
+        ciudadDeOrigen: origen,
         artista: {
-            nombre: "",
-            CancionesFavoritas: []
+            nombre: artista,
+            CancionesFavoritas: [favorito1,favorito2,favorito3]
         }
-    }
+    };
+
+
+    RadioEscuchas.push(persona);
+
+    console.log(RadioEscuchas);
 }
